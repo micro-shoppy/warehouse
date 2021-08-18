5111441b38d49b86f8d0a08305120165e46cc4e2
Rafal Kulik <rafal.kulik@intel.com>
2021-08-18 21:05:33 +0200

    Refactor: clean-up after changing project name

--- Microshoppy.Warehouse/src/Startup.cs
+++ Microshoppy.Warehouse/src/Startup.cs
@@ -26,10 +26,9 @@ namespace Microshoppy.Warehouse
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
 			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Warehouse_Service", Version = "v1" });
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Microshoppy.Warehouse", Version = "v1" });
 			});
-			services.AddMediatR(
-				typeof(CreateWarehouseProductCommandHandler));
+			services.AddMediatR(typeof(CreateWarehouseProductCommandHandler));
 			services.AddTransient<IWarehouseRepository, InMemoryWarehouseRepository>();
 		}
 
@@ -40,7 +39,7 @@ namespace Microshoppy.Warehouse
 			{
 				app.UseDeveloperExceptionPage();
 				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Warehouse_Service v1"));
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Microshoppy.Warehouse v1"));
 			}
 
 			app.UseHttpsRedirection();
